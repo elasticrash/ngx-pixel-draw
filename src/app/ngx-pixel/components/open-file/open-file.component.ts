888ef820ec38a8058ad687d8e30538dcdee13de8
unknown <s.kouroupis@gmail.com>
2017-12-23 23:06:17 +0000

    read bytes from base64

--- src/app/ngx-pixel/components/open-file/open-file.component.ts
+++ src/app/ngx-pixel/components/open-file/open-file.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SyncService } from '../../services/sync.service';
+import { DecodePngService } from '../../services/decode-png.service';
 
 @Component({
   selector: 'open-file',
@@ -7,12 +8,30 @@ import { SyncService } from '../../services/sync.service';
   styleUrls: ['./open-file.component.css']
 })
 export class OpenFileComponent implements OnInit {
+  @ViewChild('file') public file: any;
   public selectedColor;
-
+  public bytes: string[] = [];
   constructor(
+    private decodeService: DecodePngService
   ) { }
 
   public ngOnInit() {
 
+  }
+
+  public readFile(): void {
+    const reader = new FileReader();
+    const self = this;
+    reader.onloadend = (e) => {
+      self.decode(reader.result);
+    };
+    reader.readAsDataURL(this.file.nativeElement.files[0]);
+  }
+
+  public decode(base64) {
+    for (let index = 0; index < base64.length; index++) {
+      const element = base64[index];
+      this.bytes.push(this.decodeService.base64Decode(element));
+    }
   }
 }
